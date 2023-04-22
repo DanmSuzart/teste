cb99c63e1a10b3a30b2c85fc31df5eeab104764c
DanmSuzart <eudanielsuzart@gmail.com>
2023-04-22 14:32:51 -0300

    atualização3

--- html/js/script.js
+++ html/js/script.js
@@ -1,4 +1,40 @@
 
+let dados = JSON.parse(localStorage.getItem('pokemons')) ?? [];
+
+const todoList = document.querySelector("#todo-list");
+
+const saveList = (text) => {
+  
+  const todo = document.createElement("div");
+  todo.classList.add("todo");
+
+  const todoTitle = document.createElement("h3");
+  todoTitle.innerText = text;
+  todo.appendChild(todoTitle);
+
+  const editBtn = document.createElement("button");
+  editBtn.classList.add("edit-todo");
+  editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
+  todo.appendChild(editBtn);
+
+  const deletBtn = document.createElement("button");
+  deletBtn.classList.add("remove-todo");
+  deletBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+  todo.appendChild (deletBtn);
+
+  const testBtn = document.createElement("button");
+  testBtn.innerText = 'teste';
+  todo.appendChild(testBtn);
+
+  todoList.appendChild(todo);
+ 
+}
+dados.forEach((value) => {
+    if(value){
+      saveList(value);
+    }
+});
+
 function logar(){
   let login = document.getElementById('login').value;
   let senha = document.getElementById('senha').value;
@@ -12,17 +48,12 @@ function logar(){
   }
 }
 
-
-
 const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-input");
-const todoList = document.querySelector("#todo-list");
 const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 const eraseBtn = document.querySelector("#erase-button");
-
-let dados = [];
 
 //Funções
 const saveTodo = (text) => {
@@ -72,54 +103,6 @@ const saveTodo = (text) => {
     }
   });
 
-  if(localStorage != " "){
-   let dados = JSON.parse(localStorage.getItem('pokemons'));
-
-    for(i = 0; i <= dados.length; i++){
-      let dadosLista = dados[i];
-      todoForm.addEventListener("submit", (e) => {
-        e.preventDefault();
-    
-        if(dadosLista){
-        saveTodo(dadosLista);
-        }
-      });
-          
-        const saveList = (text) => {
-  
-          const todo = document.createElement("div");
-          todo.classList.add("todo");
-        
-          const todoTitle = document.createElement("h3");
-          todoTitle.innerText = text;
-          todo.appendChild(todoTitle);
-        
-          const editBtn = document.createElement("button");
-          editBtn.classList.add("edit-todo");
-          editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
-          todo.appendChild(editBtn);
-        
-          const deletBtn = document.createElement("button");
-          deletBtn.classList.add("remove-todo");
-          deletBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
-          todo.appendChild (deletBtn);
-        
-          const testBtn = document.createElement("button");
-          testBtn.innerText = 'teste';
-          todo.appendChild(testBtn);
-        
-          todoList.appendChild(todo);
-        
-          todoInput.value = "";
-          todoInput.focus();
-         
-        }
-      saveList();
-    }
-  
-  }
-
-
 document.addEventListener("click", (e) => {
   const targetEl = e.target
   const parentEl = targetEl.closest("div");
@@ -127,7 +110,5 @@ document.addEventListener("click", (e) => {
   if(targetEl.classList.contains("remove-todo")){
       parentEl.remove();
   }
-
-  
 
 });
