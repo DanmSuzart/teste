473774ba1e6b62f77d1ece3ce9ee5cbbc6e9e8d7
DanmSuzart <eudanielsuzart@gmail.com>
2023-04-22 21:12:14 -0300

    atualização4

--- html/js/script.js
+++ html/js/script.js
@@ -4,7 +4,7 @@ let dados = JSON.parse(localStorage.getItem('pokemons')) ?? [];
 const todoList = document.querySelector("#todo-list");
 
 const saveList = (text) => {
-  
+
   const todo = document.createElement("div");
   todo.classList.add("todo");
 
@@ -20,31 +20,47 @@ const saveList = (text) => {
   const deletBtn = document.createElement("button");
   deletBtn.classList.add("remove-todo");
   deletBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
-  todo.appendChild (deletBtn);
 
-  const testBtn = document.createElement("button");
-  testBtn.innerText = 'teste';
-  todo.appendChild(testBtn);
+  deletBtn.addEventListener("click", (e) => {
+    const targetEl = e.target
+    const parentEl = targetEl.closest("div");
+
+    if (targetEl.classList.contains("remove-todo")) {
+      parentEl.remove();
+
+    }
+
+    if (text) {
+
+      dados.splice(text, 1);
+      localStorage.removeItem('pokemons');
+      let data = JSON.stringify(dados);
+      localStorage.setItem('pokemons', data);
+    }
+
+  });
+
+  todo.appendChild(deletBtn);
 
   todoList.appendChild(todo);
- 
+
 }
 dados.forEach((value) => {
-    if(value){
-      saveList(value);
-    }
+  if (value) {
+    saveList(value);
+  }
 });
 
-function logar(){
+function logar() {
   let login = document.getElementById('login').value;
   let senha = document.getElementById('senha').value;
 
-  if(login == "admin" && senha == 123){
-     alert('sucesso');
-     location.href = "menu.html";
+  if (login == "admin" && senha == 123) {
+    alert('sucesso');
+    location.href = "criar.html";
 
   } else {
-     alert('Usuario ou senha incorreto');
+    alert('Usuario ou senha incorreto');
   }
 }
 
@@ -57,7 +73,7 @@ const eraseBtn = document.querySelector("#erase-button");
 
 //Funções
 const saveTodo = (text) => {
-  
+
   const todo = document.createElement("div");
   todo.classList.add("todo");
 
@@ -73,42 +89,49 @@ const saveTodo = (text) => {
   const deletBtn = document.createElement("button");
   deletBtn.classList.add("remove-todo");
   deletBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
-  todo.appendChild (deletBtn);
+  todo.appendChild(deletBtn);
 
-  const testBtn = document.createElement("button");
-  testBtn.innerText = 'teste';
-  todo.appendChild(testBtn);
+
+  deletBtn.addEventListener("click", (e) => {
+    const targetEl = e.target
+    const parentEl = targetEl.closest("div");
+
+    if (targetEl.classList.contains("remove-todo")) {
+      parentEl.remove();
+
+    }
+
+    if (text) {
+      dados.splice(text, 1);
+      localStorage.removeItem('pokemons');
+      let data = JSON.stringify(dados);
+      localStorage.setItem('pokemons', data);
+    }
+
+  });
 
   todoList.appendChild(todo);
 
   todoInput.value = "";
   todoInput.focus();
- 
+
   dados.push(text);
-  let data = JSON.stringify(dados);
+  data = JSON.stringify(dados);
   localStorage.setItem('pokemons', data);
-  
- 
+
+
 }
 
 //Eventos
 
-  todoForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+todoForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    const inputValue = todoInput.value;
+  const inputValue = todoInput.value;
 
-    if(inputValue){
+  if (inputValue) {
     saveTodo(inputValue);
-    }
-  });
-
-document.addEventListener("click", (e) => {
-  const targetEl = e.target
-  const parentEl = targetEl.closest("div");
-
-  if(targetEl.classList.contains("remove-todo")){
-      parentEl.remove();
   }
-
 });
+
+
